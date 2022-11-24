67f849726bc09483554d1534b907401913371669
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-11-24 00:01:34 -0700

    users

---
+++ users/admin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
